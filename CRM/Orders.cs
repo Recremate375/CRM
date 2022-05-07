2e5dde0eda3f93738058a06d150737a53739c0de
Alexey <leha.ignatovich.02@mail.ru>
2022-05-07 19:13:07 +0300

    Initial commit

---
+++ CRM/Orders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM
+{
+    internal class Orders
+    {
+        public DateTime OrdersDate { get; set; }
+        
+        private string OrdersName;
+        private string OrdersType;
+
+    }
+}
